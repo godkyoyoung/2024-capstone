601330036f43d0c17fc5516060d9300409608c5e
godkyoyoung <151357871+godkyoyoung@users.noreply.github.com>
2024-05-28 23:39:05 +0900

    Add files via upload

---
+++ RobotControll/RobotControll.ino
@@ -0,0 +1,59 @@
+#include "RobotController.hpp"
+
+RobotController robotController;
+
+#define BTN_PIN 10
+
+void setup()
+{
+  pinMode(BTN_PIN, INPUT);
+  Serial.begin(115200);
+  robotController = RobotController();
+}
+ 
+void loop()
+{
+  // int interrupt = digitalRead(BTN_PIN);
+  // if(interrupt == HIGH){
+  //   robotController.setIsInterrupted(
+  //     !robotController.getIsInterrupted()
+  //   );
+  // }
+  if(0){
+    robotController.stop();
+  } else {
+    readOperation(robotController);
+  }
+}
+
+void readOperation(RobotController robotController){
+  int readLen = Serial.available();
+  if(readLen){
+    for(int i = 0; i < readLen; i++){
+      char operation = Serial.read();
+      int power = Serial.parseInt();
+      Serial.println(operation);
+      Serial.println(power);
+      if (power != 0) {
+        robotController.setCurrentSpeed(power);
+      }
+      switch(operation){
+        case 'F':
+          robotController.forward();
+          break;
+        case 'B':
+          robotController.backward();
+          break;
+        case 'R':
+          robotController.right();
+          break;
+        case 'L':
+          robotController.left();
+          break;
+        case 'S':
+          robotController.stop();
+          break;
+      }
+    }
+  }
+}
---
+++ RobotControll/RobotControllerConstant.hpp
@@ -0,0 +1,5 @@
+namespace RobotControllerConstant {
+  unsigned int MAX_POWER = 255;
+  unsigned int DEFAULT_POWER = 100;
+  unsigned int MIN_POWER = 0;
+}
